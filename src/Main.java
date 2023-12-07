14f91012274da9540e22fba576b875a2b1822f25
Evgeny Kulpinov <evgen2309@yandex.ru>
2023-12-07 21:49:34 +0300

    Калькулятор попытка №1

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,32 @@ public class Main {
         }
         yaer = yaer - 2002;   // Это чтобы считывать массив с 0
         double capital = 100;
-        double indexing;
-        double percent = 0;   // Процент изъятия
+        double indexing;      // Изменение индекса в процентах
+        double percent = 0.5;   // Процент изъятия
         int i = 1;
         int s = 0;            // Обнулятор счетчика
         while (i > 0) {
+            double expenses = percent;                             // Расход
             while (yaer + 1 < Constants.MOEX_RATE.length) {
                 double index1 = (Constants.MOEX_RATE[yaer]);       // Индекс за текущий год
                 double index2 = (Constants.MOEX_RATE[yaer + 1]);   // Индекс за следующий год
                 double inflation = (Constants.INFLATION_RATE[yaer]);
-                capital = capital - percent;
-                indexing = index2 / index1;                        // Изменение индекса в процентах
+                capital = capital - expenses;
+                indexing = index2 / index1;
                 capital = capital * indexing;
-                capital = capital * (1 - inflation / 100);
+                expenses = expenses * (1 + inflation / 100);       // Увеличение расхода инфляцией
                 yaer++;
                 s++;
             }
             yaer = yaer - s;
             s = 0;
             percent = percent + 0.5;
-            if (capital <= 0) {
+            if (capital < 0) {
                 i = -1;
             }
             capital = 100;
         }
-        percent = percent - 0.5;
+        percent = percent - 1;
         System.out.println(percent);
     }
 }
