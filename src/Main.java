d130e6f0b59e638c7838bf51eab0912580bfff04
Evgeny Kulpinov <evgen2309@yandex.ru>
2023-12-10 18:28:34 +0300

    Калькулятор попытка №1

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int yaer = console.nextInt();
-        if (yaer > 2021 | yaer < 2002) {
+        int year = console.nextInt();
+        fireCalculation(year);
+    }
+
+    public static void fireCalculation(int year) {
+        double percent = 0.5;
+        double percentageChange = 0.5;
+        int startYear = 2002;
+        int finalYear = 2021;
+
+        if (year > finalYear | year < startYear) {
             System.err.println("Введите дату в диапазоне 2002-2021 включительно");
             return;
         }
-        yaer = yaer - 2002;   // Это чтобы считывать массив с 0
+        year = year - startYear;
+        percent = calculationInterest(year, percent, percentageChange);
+        outputResult(percent, percentageChange);
+    }
+
+    public static double calculationInterest(int year, double percent, double percentageChange) {
         double capital = 100;
-        double indexing;      // Изменение индекса в процентах
-        double percent = 0.5;   // Процент изъятия
-        int i = 1;
-        int s = 0;            // Обнулятор счетчика
-        while (i > 0) {
-            double expenses = percent;                             // Расход
-            while (yaer + 1 < Constants.MOEX_RATE.length) {
-                double index1 = (Constants.MOEX_RATE[yaer]);       // Индекс за текущий год
-                double index2 = (Constants.MOEX_RATE[yaer + 1]);   // Индекс за следующий год
-                double inflation = (Constants.INFLATION_RATE[yaer]);
+        double expenses;
+        double indexing;
+        int zeroingTheCounter = 0;
+        boolean positiveCapital = true;
+
+        while (positiveCapital) {
+            expenses = percent;
+            while (year + 1 < Constants.MOEX_RATE.length) {
                 capital = capital - expenses;
-                indexing = index2 / index1;
+                indexing = (Constants.MOEX_RATE[year + 1]) / (Constants.MOEX_RATE[year]);
                 capital = capital * indexing;
-                expenses = expenses * (1 + inflation / 100);       // Увеличение расхода инфляцией
-                yaer++;
-                s++;
+                expenses = expenses * (1 + (Constants.INFLATION_RATE[year]) / 100);
+                year++;
+                zeroingTheCounter++;
             }
-            yaer = yaer - s;
-            s = 0;
-            percent = percent + 0.5;
+            year = year - zeroingTheCounter;
+            zeroingTheCounter = 0;
+            percent = percent + percentageChange;
             if (capital < 0) {
-                i = -1;
+                positiveCapital = false;
             }
             capital = 100;
         }
-        percent = percent - 1;
+        return percent;
+    }
+
+    public static void outputResult(double percent, double percentageChange) {
+        percent = percent - percentageChange - percentageChange;
         System.out.println(percent);
     }
 }
